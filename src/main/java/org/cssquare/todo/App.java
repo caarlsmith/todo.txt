8959173c150e93d3f19b292fc5f5a8b8f76788a7
Nicole Stewart <nicole.deanna@hotmail.com>
2013-03-21 20:29:25 -0300

    fixed error

--- src/main/java/org/cssquare/todo/App.java
+++ src/main/java/org/cssquare/todo/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args){
     	try {
 	      	File f = new File ("todo.txt");
-	      	ArrayList <Task> taskList = FileHandler.readfile(f);
+	      	ArrayList<Task> taskList = FileHandler.readfile(f);
 	      	CommandResult result = CommandHandler.dispatch(args, taskList);
 	      	FileHandler.write(result.getTaskList());
 	      	System.out.println(result.getMessage());
